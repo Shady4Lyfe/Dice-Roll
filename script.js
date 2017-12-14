349ca0d298ee926ad7f75303a2017cb4d730ffb7
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-12-14 12:08:13 -0700

    dawghwa

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Dice Roll Simulator
 
+// Declare Variables
+let sum2 = 0
+let sum3 = 0
+let sum4 = 0
+let sum5 = 0
+let sum6 = 0
+let sum7 = 0
+let sum8 = 0
+let sum9 = 0
+let sum10 = 0
+let sum11 = 0
+let sum12 = 0
+
+
 // Add Event Listeners
 $("#roll").click(diceRoll);
 
@@ -13,7 +27,51 @@ function diceRoll() {
     let dice1 = Math.floor(Math.random() * 6) + 1;
     let dice2 = Math.floor(Math.random() * 6) + 1;
     $(".output").append("(" + dice1 + "," + dice2 + ")" + "<br>");
-    
+
+    if (dice1 + dice2 == 2) {
+      $("#sums").html("Sum of 2: " + (sum2 + 1));
+      sum2++;
+    }
+
+    if (dice1 + dice2 == 3) {
+      sum3++;
+    }
+
+    if (dice1 + dice2 == 4) {
+      sum4++;
+    }
+
+    if (dice1 + dice2 == 5) {
+      sum5++;
+    }
+
+    if (dice1 + dice2 == 6) {
+      sum6++;
+    }
+
+    if (dice1 + dice2 == 7) {
+      sum7++;
+    }
+
+    if (dice1 + dice2 == 8) {
+      sum8++;
+    }
+
+    if (dice1 + dice2 == 9) {
+      sum9++;
+    }
+
+    if (dice1 + dice2 == 10) {
+      sum10++;
+    }
+
+    if (dice1 + dice2 == 11) {
+      sum11++;
+    }
+
+    if (dice1 + dice2 == 12) {
+      sum12++;
+    }
   }
-  
+
 }
